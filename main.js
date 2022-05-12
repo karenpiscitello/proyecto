924d6555fe2faa38f924902a1f796f0052e53e68
karenpiscitello <karen.piscitello23@gmail.com>
2022-05-11 23:51:57 -0300

    modifico

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 const express = require('express')
-const Contenedor = require('')
+const Contenedor = require('proyecto/main')
 
 const app = express()
 
 const PORT = 8080
 
-app.get('', (req, res) => {
-  res.send({ mensaje: 'Bienvenidos al curso'})
+app.get('', (require, resp) => {
+    resp.send({ mensaje: 'Bienvenidos al curso'})
 })
 
-app.get('/productos', (req, res) => {
-  const contenedor = new Contenedor('productos.txt')
-  contenedor.getRandom()
-  res.send('Endpoint de productos')
+app.get('/productos', (require, resp) => {
+    const contenedor = new Contenedor('productos.txt')
+    contenedor.getRandom()
+    resp.send('Endpoint de productos')
 })
 
-app.get('/visitas', (req, res) => {
-  res.send('Endpoint de visitas')
+app.get('/randomproductos', (require, resp) => {
+    resp.send('Endpoint de random productos')
 })
 
 const server = app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`)
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
 })
 
 server.on('error', (error) => console.log(`Error en servidor: ${error}`))
