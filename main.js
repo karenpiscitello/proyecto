8fb7bef55ebaede1d6c7d68b51dc079a27aa9d4d
karenpiscitello <karen.piscitello23@gmail.com>
2022-05-11 23:44:58 -0300

    modif

--- main.js
+++ main.js
@@ -1,50 +1,26 @@
-const productos = [
-    { id:1, nombre:'Escuadra', precio:323.45 },
-    { id:2, nombre:'Calculadora', precio:234.56 },
-    { id:3, nombre:'Globo Terráqueo', precio:45.67 },
-    { id:4, nombre:'Paleta Pintura', precio:456.78 },
-    { id:5, nombre:'Reloj', precio:67.89 },
-    { id:6, nombre:'Agenda', precio:78.90 }
-  ]
-  
-  let nombres = [];
-  
-  productos.forEach(elem => nombres.push(elem.nombre));
-  
-  console.log(nombres);
-  
-  let precioTotal = 0;
-  
-  productos.forEach(elem => precioTotal += elem.precio);
-  
-  console.log(precioTotal);
-  
-  let promedio = precioTotal / productos.length;
-  
-  console.log(promedio);
-  
-  let menorPrecio = productos[0];
-  
-  productos.forEach(elem => {
-    if (elem.precio <= menorPrecio.precio) {
-        menorPrecio = elem;
-    }
-  } );
-  
-  console.log(menorPrecio);
-  
-  let mayorPrecio = productos[0];
-  
-  productos.forEach(elem => {
-    if (elem.precio >= mayorPrecio.precio) {
-        mayorPrecio = elem;
-    }
-  } );
-  
-  console.log({
-    nombres,
-    precioTotal,
-    promedio,
-    menorPrecio,
-    mayorPrecio
-  })
+const express = require('express')
+const Contenedor = require('')
+
+const app = express()
+
+const PORT = 8080
+
+app.get('', (req, res) => {
+  res.send({ mensaje: 'Bienvenidos al curso'})
+})
+
+app.get('/productos', (req, res) => {
+  const contenedor = new Contenedor('productos.txt')
+  contenedor.getRandom()
+  res.send('Endpoint de productos')
+})
+
+app.get('/visitas', (req, res) => {
+  res.send('Endpoint de visitas')
+})
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
+
+server.on('error', (error) => console.log(`Error en servidor: ${error}`))
